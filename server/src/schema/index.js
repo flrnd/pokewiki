23e1ddf61ba5ea8aa27879d41350847a627fdf74
Florian Rand <florian.rand@gmail.com>
2019-10-02 11:47:22 +0200

    Remove babel-graphql-import and fix errors

--- server/src/schema/index.js
+++ server/src/schema/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { makeExecutableSchema } from 'graphql-tools';
 import { resolvers } from '../resolvers';
 
-const typeDefs = fs.readFileSync('./schema.graphql', 'utf8');
+const typeDefs = fs.readFileSync('./src/schema/schema.graphql', 'utf8');
 
 const schema = makeExecutableSchema({
   typeDefs,
