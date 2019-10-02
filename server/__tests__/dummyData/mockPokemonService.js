5c6d30f0928cdd2b4a7702dc195170a2af7cb9fa
Florian Rand <florian.rand@gmail.com>
2019-10-02 15:16:29 +0200

    Refactor mockPokemonService.js
    There was no need to all that file read, solved with a simple es6 import
    :)

--- server/__tests__/dummyData/mockPokemonService.js
+++ server/__tests__/dummyData/mockPokemonService.js
@@ -2,25 +2,10 @@
 // Because the pokemon response is quite big, there are
 // a few json examples directly from the API.
 
-import fs from 'fs';
-import path from 'path';
+import { bulbasaur, charmeleon, ekans } from './mocks';
 
-const jsonFolder = './json/';
-const defaultPokemon = { name: 'bulbasaur.json', folder: jsonFolder };
-
-const getAllFilesFromFolder = folder =>
-  fs.readdirSync(folder).map(file => path.resolve(folder, file));
-
-const getAllPokemonsFromFileList = fileList =>
-  fileList.map(file => JSON.parse(fs.readFileSync(file)));
-
-const getPokemonByFileName = pokemonFile => {
-  const file = path.resolve(pokemonFile.folder, pokemonFile.name);
-  return JSON.parse(fs.readFileSync(file));
-};
-
-const pokemons = getAllPokemonsFromFileList(getAllFilesFromFolder(jsonFolder));
-const pokemon = getPokemonByFileName(defaultPokemon);
+pokemons = [bulbasaur, charmeleon, ekans];
+pokemon = bulbasaur;
 
 export const allPokemons = () => pokemons;
 export const getPokemon = () => pokemon;
