2bfc7817039b0aa875ac4df7ebfd8d61584d9f80
Florian Rand <florian.rand@gmail.com>
2019-10-02 14:59:06 +0200

    Add tests dir and first file

---
+++ server/__tests__/pokemon.test.js
@@ -0,0 +1,52 @@
+import { makeExecutableSchema } from 'graphql-tools';
+import { graphql } from 'graphql';
+import resolvers from '../src/resolvers';
+import mockPokemonService from './mocks/mockPokemonService';
+import pokemon from './mocks/';
+
+const PokemonTestCase = {
+  id: 'Pokemon test case',
+  query: `
+    query {
+      pokemon(id: 1) {
+    id
+    name
+    pokemonTypes {
+      slot
+      name
+    }
+    height
+    weight
+    species {
+      name
+      color
+      description
+      baseHappiness
+      captureRate
+      genera
+      growthRate
+      habitat
+      hatchCounter
+    }
+    stats {
+      name
+      baseStat
+      effort
+    }
+    abilities {
+      slot
+      isHidden
+      name
+    }
+    moves {
+      name
+      levelLearnedAt
+      moveLearnMethod
+      versionGroup
+    }
+  }
+  `,
+  variables: {},
+  context: { pokemonService: mockPokemonService },
+  expected: { data: { pokemon } },
+};
