6e9e328f59c3fe6afbba0ac4680aafa9bb838f33
Florian Rand <florian.rand@gmail.com>
2019-10-04 16:38:08 +0200

    Add unit tests

--- server/__tests__/pokemonApi.test.js
+++ server/__tests__/pokemonApi.test.js
@@ -4,22 +4,31 @@ import * as rawApiCall from './dummyData/mocks/1.json';
 
 describe('PokemonAPI', () => {
   const myDataSource = new PokemonsAPI();
-  let result = '';
+  const result = myDataSource.get('pokemon', '1');
 
-  //  beforeEach(() => {
-  //  const spy = jest
-  //    .spyOn(myDataSource, 'get')
-  //    .mockImplementation((type, id) => rawApiCall);
-  //  result = myDataSource.get('pokemon', '1');
-  //});
+  beforeEach(() => {
+    const spy = jest
+      .spyOn(myDataSource, 'get')
+      .mockImplementation(param => rawApiCall);
+  });
 
-  //it('calls get()', () => {
-  //  expect(myDataSource.get).toHaveBeenCalledTimes(1);
-  //});
+  it('calls get()', () => {
+    expect(myDataSource.get).toHaveBeenCalledTimes(1);
+  });
 
-  it('get object by type and id', async () => {
-    expect.assertions(1);
-    const result = await myDataSource.getObjectByTypeAndId('pokemon', '1');
-    expect(result).toMatchObject(bulbasaur);
+  test('getObjectByTypeAndId returns  an api request', async () => {
+    await expect(
+      myDataSource.getObjectByTypeAndId('pokemon', '1'),
+    ).resolves.toMatchObject(rawApiCall);
+  });
+
+  test('getAllTypes returns a pokemon-types array from a result', () => {
+    expect(myDataSource.getAllTypes(result.types)).toMatchObject(bulbasaur);
+  });
+
+  test('getPokemonByURL returns a pokemon', async () => {
+    await expect(
+      myDataSource.getPokemonByURL('mocked/api/url'),
+    ).resolves.toMatchObject(bulbasaur);
   });
 });
