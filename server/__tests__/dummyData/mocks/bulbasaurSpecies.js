502d48ca46aea5e888e73a7970f27335a39b6164
Florian Rand <florian.rand@gmail.com>
2019-10-07 17:49:41 +0200

    Add bulbasaur Species mock object

---
+++ server/__tests__/dummyData/mocks/bulbasaurSpecies.js
@@ -0,0 +1,12 @@
+export const bulbasaurSpecies = {
+  name: 'bulbasaur',
+  color: 'green',
+  description:
+    'Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun’s rays, the seed grows progressively larger.',
+  genera: 'Seed Pokémon',
+  baseHappiness: 70,
+  captureRate: 45,
+  growthRate: 'medium-slow',
+  habitat: 'grassland',
+  hatchCounter: 20,
+};
