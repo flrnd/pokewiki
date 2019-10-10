16f6226eb116befc7b1f52d567b465d4f02a170b
Florian Rand <florian.rand@gmail.com>
2019-10-10 22:09:36 +0200

    Add eslint-airbnb and prettier

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,20 +1,4 @@
-const path = require('path');
-
 module.exports = {
   parser: 'babel-eslint',
-  rules: {
-    'graphql/capitalized-type-name': 'error',
-    'graphql/named-operations': 'error',
-    'graphql/no-deprecated-fields': 'error',
-    'graphql/template-strings': [
-      'error',
-      {
-        env: 'apollo',
-        //schemaJsonFilepath: path.resolve(__dirname, './src/schema/schema.json'),
-        // OR provide the schema in the Schema Language format
-        //schemaString: printSchema('./schema.graphql'),
-      },
-    ],
-  },
-  plugins: ['graphql'],
+  extends: ['airbnb', 'plugin:prettier/recommended'],
 };
